333ad2cb9eb2833a34d2b53a48fdf26f7af86530
Marcos <marcosjuca1@hotmail.com>
2021-10-02 10:39:56 -0300

    FATEC

---
+++ Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace ProvaProgramação
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbNumero1 = new System.Windows.Forms.Label();
+            this.lbNumero2 = new System.Windows.Forms.Label();
+            this.numero1 = new System.Windows.Forms.TextBox();
+            this.numero2 = new System.Windows.Forms.TextBox();
+            this.btAdicao = new System.Windows.Forms.Button();
+            this.btSubtracao = new System.Windows.Forms.Button();
+            this.btMultiplicacao = new System.Windows.Forms.Button();
+            this.btDivisao = new System.Windows.Forms.Button();
+            this.lbResultado1 = new System.Windows.Forms.Label();
+            this.lbresultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbNumero1
+            // 
+            this.lbNumero1.AutoSize = true;
+            this.lbNumero1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNumero1.Location = new System.Drawing.Point(54, 65);
+            this.lbNumero1.Name = "lbNumero1";
+            this.lbNumero1.Size = new System.Drawing.Size(83, 23);
+            this.lbNumero1.TabIndex = 0;
+            this.lbNumero1.Text = "Número 1:";
+            // 
+            // lbNumero2
+            // 
+            this.lbNumero2.AutoSize = true;
+            this.lbNumero2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNumero2.Location = new System.Drawing.Point(54, 112);
+            this.lbNumero2.Name = "lbNumero2";
+            this.lbNumero2.Size = new System.Drawing.Size(86, 23);
+            this.lbNumero2.TabIndex = 1;
+            this.lbNumero2.Text = "Número 2:";
+            // 
+            // numero1
+            // 
+            this.numero1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero1.Location = new System.Drawing.Point(143, 62);
+            this.numero1.Name = "numero1";
+            this.numero1.Size = new System.Drawing.Size(374, 30);
+            this.numero1.TabIndex = 2;
+            // 
+            // numero2
+            // 
+            this.numero2.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numero2.Location = new System.Drawing.Point(143, 109);
+            this.numero2.Name = "numero2";
+            this.numero2.Size = new System.Drawing.Size(374, 30);
+            this.numero2.TabIndex = 3;
+            // 
+            // btAdicao
+            // 
+            this.btAdicao.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btAdicao.Location = new System.Drawing.Point(58, 169);
+            this.btAdicao.Name = "btAdicao";
+            this.btAdicao.Size = new System.Drawing.Size(109, 35);
+            this.btAdicao.TabIndex = 4;
+            this.btAdicao.Text = "Adição";
+            this.btAdicao.UseVisualStyleBackColor = true;
+            this.btAdicao.Click += new System.EventHandler(this.btAdicao_Click);
+            // 
+            // btSubtracao
+            // 
+            this.btSubtracao.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btSubtracao.Location = new System.Drawing.Point(173, 169);
+            this.btSubtracao.Name = "btSubtracao";
+            this.btSubtracao.Size = new System.Drawing.Size(109, 35);
+            this.btSubtracao.TabIndex = 5;
+            this.btSubtracao.Text = "Subtração";
+            this.btSubtracao.UseVisualStyleBackColor = true;
+            this.btSubtracao.Click += new System.EventHandler(this.btSubtracao_Click);
+            // 
+            // btMultiplicacao
+            // 
+            this.btMultiplicacao.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btMultiplicacao.Location = new System.Drawing.Point(293, 169);
+            this.btMultiplicacao.Name = "btMultiplicacao";
+            this.btMultiplicacao.Size = new System.Drawing.Size(109, 35);
+            this.btMultiplicacao.TabIndex = 6;
+            this.btMultiplicacao.Text = "Multiplicação";
+            this.btMultiplicacao.UseVisualStyleBackColor = true;
+            this.btMultiplicacao.Click += new System.EventHandler(this.btMultiplicacao_Click);
+            // 
+            // btDivisao
+            // 
+            this.btDivisao.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btDivisao.Location = new System.Drawing.Point(408, 169);
+            this.btDivisao.Name = "btDivisao";
+            this.btDivisao.Size = new System.Drawing.Size(109, 35);
+            this.btDivisao.TabIndex = 7;
+            this.btDivisao.Text = "Divisão";
+            this.btDivisao.UseVisualStyleBackColor = true;
+            this.btDivisao.Click += new System.EventHandler(this.btDivisao_Click);
+            // 
+            // lbResultado1
+            // 
+            this.lbResultado1.AutoSize = true;
+            this.lbResultado1.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbResultado1.Location = new System.Drawing.Point(54, 258);
+            this.lbResultado1.Name = "lbResultado1";
+            this.lbResultado1.Size = new System.Drawing.Size(88, 23);
+            this.lbResultado1.TabIndex = 8;
+            this.lbResultado1.Text = "Resultado:";
+            // 
+            // lbresultado
+            // 
+            this.lbresultado.AutoSize = true;
+            this.lbresultado.Font = new System.Drawing.Font("Bahnschrift SemiCondensed", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbresultado.Location = new System.Drawing.Point(148, 258);
+            this.lbresultado.Name = "lbresultado";
+            this.lbresultado.Size = new System.Drawing.Size(18, 23);
+            this.lbresultado.TabIndex = 9;
+            this.lbresultado.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(586, 387);
+            this.Controls.Add(this.lbresultado);
+            this.Controls.Add(this.lbResultado1);
+            this.Controls.Add(this.btDivisao);
+            this.Controls.Add(this.btMultiplicacao);
+            this.Controls.Add(this.btSubtracao);
+            this.Controls.Add(this.btAdicao);
+            this.Controls.Add(this.numero2);
+            this.Controls.Add(this.numero1);
+            this.Controls.Add(this.lbNumero2);
+            this.Controls.Add(this.lbNumero1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbNumero1;
+        private System.Windows.Forms.Label lbNumero2;
+        private System.Windows.Forms.TextBox numero1;
+        private System.Windows.Forms.TextBox numero2;
+        private System.Windows.Forms.Button btAdicao;
+        private System.Windows.Forms.Button btSubtracao;
+        private System.Windows.Forms.Button btMultiplicacao;
+        private System.Windows.Forms.Button btDivisao;
+        private System.Windows.Forms.Label lbResultado1;
+        private System.Windows.Forms.Label lbresultado;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ProvaProgramação
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btAdicao_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(numero1.Text);
+            double num2 = Convert.ToDouble(numero2.Text);
+            string result = (num1 + num2).ToString();
+            lbresultado.Text = result;
+        }
+
+        private void btSubtracao_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(numero1.Text);
+            double num2 = Convert.ToDouble(numero2.Text);
+            string result = (num1 - num2).ToString();
+            lbresultado.Text = result;
+        }
+
+        private void btMultiplicacao_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(numero1.Text);
+            double num2 = Convert.ToDouble(numero2.Text);
+            string result = (num1 * num2).ToString();
+            lbresultado.Text = result;
+        }
+
+        private void btDivisao_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(numero1.Text);
+            double num2 = Convert.ToDouble(numero2.Text);
+            string result = (num1 / num2).ToString();
+            lbresultado.Text = result;
+        }
+    }
+}
